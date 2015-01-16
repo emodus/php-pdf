0e1c7c7637a40bd479835f964d6cddbb6ac015a7
emodus <emodus@users.noreply.github.com>
2015-01-16 14:00:23 +0200

    Update PDF.php
    With large set pdf more than 40+ pages on windows machine process hangs on stderr stream.

--- src/CanGelis/PDF/PDF.php
+++ src/CanGelis/PDF/PDF.php
@@ -228,7 +228,7 @@ class PDF {
 		$descriptorspec = array(
 			0 => array("pipe", "r"), // stdin is a pipe that the child will read from
 			1 => array("pipe", "w"), // stdout is a pipe that the child will write to
-			2 => array("pipe", "w") // stderr is a pipe that the child will write to
+			2 => array("pipe", "r") // stderr is a pipe that the child will write to
 		);
 
 		$process = proc_open($this->cmd . ' ' . $this->getParams() . ' ' . $this->getInputSource() . ' ' . $this->getPDFPath(), $descriptorspec, $pipes);
